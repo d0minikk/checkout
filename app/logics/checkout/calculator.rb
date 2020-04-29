f36840977facd7117c8a07dc8a5bd46c354de665
d0minikk <curylo.dominik@gmail.com>
2020-04-29 10:45:53 +0200

    Create calculator.rb

---
+++ app/logics/checkout/calculator.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+module Checkout
+  class Calculator
+    attr_reader :total
+
+    def initialize(cart, available_promotions)
+      @cart = cart
+      @available_promotions = available_promotions
+    end
+
+    def call
+      calculate_total_adjustment
+      calculate_total
+    end
+
+    private
+
+    attr_reader :cart, :available_promotions, :items_adjustment, :order_adjustment
+
+    def calculate_total
+      @total = total_without_adjustment - total_adjustment
+    end
+
+    def total_without_adjustment
+      cart.items.map(&:price_cents).sum / 100
+    end
+
+    def total_adjustment
+      items_adjustment + order_adjustment
+    end
+
+    def calculate_total_adjustment
+      @items_adjustment = calculate_items_adjustment
+      @order_adjustment = calculate_order_adjustment
+    end
+
+    def calculate_items_adjustment
+      accumulator = 0
+
+      cart.items.map do |item|
+        available_promotions.each do |promotion|
+          if validator.eligible?(promotion, item)
+            # apply action calculation
+          end
+        end
+      end
+
+      accumulator
+    end
+
+    def calculate_order_adjustment
+      accumulator = 0
+
+      available_promotions.each do |promotion|
+        if validator.eligible?(promotion, cart, total: items_adjustment)
+          # apply action calculation
+        end
+      end
+
+      accumulator
+    end
+
+    def validator
+      @validator ||= Promotions::Eligibility::Validator.new(available_promotions)
+    end
+  end
+end
