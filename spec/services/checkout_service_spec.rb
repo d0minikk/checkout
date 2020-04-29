64e48a1daf1e44b511f15f0fa60f1da86d681428
d0minikk <curylo.dominik@gmail.com>
2020-04-29 09:07:39 +0200

    Add specs for checkout

---
+++ spec/services/checkout_service_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe CheckoutService do
+  let!(:promotions) { Promotion.all }
+  let!(:product_1) { Product.find('001') }
+  let!(:product_2) { Product.find('002') }
+  let!(:product_3) { Product.find('003') }
+
+  subject { described_class.new(promotions) }
+
+  it 'order is discounted' do
+    subject.scan(product_1)
+    subject.scan(product_2)
+    subject.scan(product_3)
+
+    expect(subject.total).to eq(66.78)
+  end
+
+  it 'product is discounted' do
+    subject.scan(product_1)
+    subject.scan(product_3)
+    subject.scan(product_1)
+
+    expect(subject.total).to eq(36.95)
+  end
+
+  it 'product and order is discounted' do
+    subject.scan(product_1)
+    subject.scan(product_2)
+    subject.scan(product_1)
+    subject.scan(product_3)
+
+    expect(subject.total).to eq(73.76)
+  end
+end
